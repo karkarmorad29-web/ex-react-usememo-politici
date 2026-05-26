a7f23955d4e75c263aeb3c09ef71c0658136da78
morad <KARKARMORAD29@GMAIL.COM>
2026-05-26 11:33:38 +0200

    useMemo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useMemo } from 'react'
 
 function App() {
   const [politicians, setPoliticians] = useState([])
+  const [search, setSearch] = useState('')
+
 
   const handleImageError = (e) => {
     e.target.src = 'https://via.placeholder.com/280x280?text=No+Image'
@@ -14,13 +16,24 @@ function App() {
       .catch(error => console.error(error));
   }, []);
 
-  console.log(politicians);
+  const filteredPoliticians = useMemo(() => {
+    return politicians.filter(politician => {
+      const isInName = politician.name.toLowerCase().includes(search.toLowerCase());
+      const isInBio = politician.biography.toLowerCase().includes(search.toLowerCase());
+      return isInName || isInBio;
+    });
+  }, [politicians, search]);
 
   return (
     <div>
       <h1>lista Politici</h1>
+      <input type="text"
+        placeholder='Cerca per nome o biografia'
+        value={search}
+        onChange={e => setSearch(e.target.value)}
+      />
       <div className='politicians-list'>
-        {politicians.map(politician => (
+        {filteredPoliticians.map(politician => (
           <div className='card' key={politician.id}>
             <img src={politician.image} alt={politician.name} onError={handleImageError} />
             <h2>{politician.name}</h2>
