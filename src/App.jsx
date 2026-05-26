5ca5a9843bfa8b1027aed617d936c95b5a370e22
morad <KARKARMORAD29@GMAIL.COM>
2026-05-26 10:04:28 +0200

    lista politici, e index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState, useEffect } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [politicians, setPoliticians] = useState([])
+
+  const handleImageError = (e) => {
+    e.target.src = 'https://via.placeholder.com/280x280?text=No+Image'
+  }
+
+  useEffect(() => {
+    fetch('http://localhost:3333/politicians')
+      .then(res => res.json())
+      .then(data => setPoliticians(data))
+      .catch(error => console.error(error));
+  }, []);
+
+  console.log(politicians);
 
   return (
-    <>
-      <h1>ciao</h1>
-    </>
+    <div>
+      <h1>lista Politici</h1>
+      <div className='politicians-list'>
+        {politicians.map(politician => (
+          <div className='card' key={politician.id}>
+            <img src={politician.image} alt={politician.name} onError={handleImageError} />
+            <h2>{politician.name}</h2>
+            <p><strong>Posizione:</strong>{politician.position}</p>
+            <p>{politician.biography}</p>
+          </div>
+        ))}
+
+      </div>
+    </div>
+
+
   )
 }
 
